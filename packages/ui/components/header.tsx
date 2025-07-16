3b9df5fa4d21a4ef996b6ea4158272e362b5bd9d
Michael-Gray_dfa <michael.gray@dimensional.com>
2025-07-16 15:33:53 -0500

    Added Vite web project with React 19

---
+++ packages/ui/components/header.tsx
@@ -0,0 +1,12 @@
+
+interface HeaderProps {
+  title: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header id="header">
+      <h1>{title}</h1>
+    </header>
+  );
+};
